a89059bac603a8a7acc8216276e1f37d8e03c5cd
FliPPeDround <734243792@qq.com>
2023-08-07 17:42:50 +0800

    chore: tsup publicDir

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,21 +1,12 @@
-import { copyFileSync } from 'node:fs'
-import { resolve } from 'node:path'
 import { bold, gray, green, yellow } from 'kolorist'
 import { defineConfig } from 'tsup'
 import pkg from './package.json'
-
-function moveTypes() {
-  const src = resolve(__dirname, 'src/types/index.d.ts')
-  const dist = resolve(__dirname, 'dist/index.d.ts')
-
-  copyFileSync(src, dist)
-}
 
 export default defineConfig ((options) => {
   const buildBanner = `\n ${yellow('⬘')} ${green('⬗')} ${yellow('⬙')} ${green('⬖')}  ${bold('leafer-vue')} ${gray(`v${pkg.version}`)} \n`
 
   return {
-    entry: ['./src/index.ts', './src/types/index.d.ts'],
+    entry: ['./src/index.ts'],
     format: options.watch ? 'esm' : ['cjs', 'esm', 'iife'],
     target: 'node14',
     splitting: true,
@@ -27,8 +18,8 @@ export default defineConfig ((options) => {
       'lodash-es',
     ],
     minify: !options.watch,
+    publicDir: 'src/types',
     onSuccess: async () => {
-      moveTypes()
       // eslint-disable-next-line no-console
       console.log(buildBanner)
       return Promise.resolve()

deab6e2e8c0f403aa6039f0980c29ca7e6937358
FliPPeDround <734243792@qq.com>
2023-08-08 12:31:11 +0800

    chore: init docs

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -2,11 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
-app.directive('focus', (el, binding) => {
-  console.log(binding)
-})
-app.directive('demo', (el, binding) => {
-  console.log(binding.value.color) // => "white"
-  console.log(binding.value.text) // => "hello!"
-})
 app.mount('#app')

3324857493d15b1810cde9c3d52138b7c8a8c46f
FliPPeDround <734243792@qq.com>
2023-08-10 17:41:18 +0800

    docs: add theme file

---
+++ docs/.vitepress/components/guide/app/app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { lfApp, lfLeafer } from './../../../dist/index.js'
+</script>
+
+<template>
+  <lfApp :width="500" :height="500" fill="#000">
+    <lfLeafer :width="100" :height="100" fill="#fff" />
+  </lfApp>
+</template>
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,10 @@
+import DefaultTheme from 'vitepress/theme'
+
+// import AppGuide from '../components/guide/app/app.vue'
+
+export default {
+  ...DefaultTheme,
+  enhanceApp: async ({ app }) => {
+    // app.component('AppGuide', AppGuide)
+  },
+}

3558bb63ee5188660513bee311332c7e658b9f23
FliPPeDround <734243792@qq.com>
2024-09-05 18:00:07 +0800

    docs: update event

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -117,6 +117,15 @@ export default defineConfig({
           },
         ],
       },
+      {
+        text: '事件',
+        items: [
+          {
+            text: '事件处理',
+            link: '/guide/events/index',
+          },
+        ],
+      },
     ],
 
     socialLinks: [

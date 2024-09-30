fd0ae9d824de35c824ef49fb331ddf4ef28c40d0
flippedround <734243792@qq.com>
2024-09-30 12:35:19 +0800

    chore: fix deploy error

--- packages/vue3/tsup.config.ts
+++ packages/vue3/tsup.config.ts
@@ -5,19 +5,21 @@ import pkg from './package.json'
 
 export default defineConfig ((options) => {
   const buildBanner = `\n ☘️ ☘️ ☘️  ${bold('leafer-vue@3')} ${gray(`v${pkg.version}`)} \n`
-
+  const isWatch = options.watch
   return {
     entry: ['index.ts', 'compiler.ts'],
     format: ['cjs', 'esm'],
     target: 'node18',
     tsconfig: './tsconfig.json',
     clean: true,
-    minify: !options.watch,
+    minify: !isWatch,
     dts: true,
     onSuccess: async () => {
       const leaferVueEsm = fs.readFileSync('./dist/index.js', 'utf-8')
       fs.writeFileSync('./../../docs/public/leafer-vue.proxy.js', leaferVueEsm)
-      fs.writeFileSync('./../../playground/public/leafer-vue.proxy.js', leaferVueEsm)
+      if (isWatch) {
+        fs.writeFileSync('./../../playground/src/leafer-vue.proxy.js', leaferVueEsm)
+      }
       // eslint-disable-next-line no-console
       console.log(buildBanner)
       return Promise.resolve()
